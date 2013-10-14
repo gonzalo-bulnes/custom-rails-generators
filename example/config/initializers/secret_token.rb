0a39355719a43f6b1779315a154442a65839d7dc
Gonzalo Bulnes Guilpain <gon.bulnes@gmail.com>
2013-10-13 22:07:08 -0300

    Add Rails as development dependency, example app
    The example application will allow to test the gem, it was generated
    by `rails new example --skip-test-unit` to be used with RSpec.

---
+++ example/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Example::Application.config.secret_token = 'a7209bc7f607d5e0f07465cc4949c34c43ab091884822934f41208a3c8dd05f20d98227bc69e872b5eb3b4c791c7d97e8fad8d2cd97c57933eddbf54a2a55a18'
